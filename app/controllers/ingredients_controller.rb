235a15a03b7629bfddd083c73a8979b2fb21be8f
werdlerk <koen@werdler.com>
2015-01-20 22:08:18 +0100

    Added new and edit forms for finalizing management of ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,7 +3,6 @@ class IngredientsController < ApplicationController
   PAGE_SIZE = 10
 
   def index
-
     respond_to do |format|
       format.js { 
         ingredients = Ingredient.order(:name).where("name like ?", "%#{params[:term]}%")
@@ -13,9 +12,51 @@ class IngredientsController < ApplicationController
         @sort = params[:sort] ? params[:sort] : 'name'
         @page = params[:page] ? (params[:page].to_i - 1) : 0
         @total = Ingredient.all.size
-        @ingredients = Ingredient.order(@sort.to_sym).offset(@page * PAGE_SIZE).limit(PAGE_SIZE)
+        @ingredients = Ingredient.order("lower(#{@sort})").offset(@page * PAGE_SIZE).limit(PAGE_SIZE)
       }
     end    
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      flash[:notice] = "Ingredient saved"
+      redirect_to ingredients_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    ingredient = Ingredient.find(params[:id])
+
+    if ingredient.update(ingredient_params)
+      flash[:notice] = "Ingredient updated"
+      redirect_to ingredients_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    ingredient = Ingredient.find(params[:id]).destroy
+    flash[:notice] = "Ingredient '#{ingredient.name}' removed"
+    redirect_to :back
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :description)
   end
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,15 @@
 class Ingredient < ActiveRecord::Base
   has_many :necessities
   has_many :images, as: :imageable
+
+  validates :name, presence: true
+
+  before_save :name_to_downcase
+
+
+  private
+
+  def name_to_downcase
+    self.name = name.downcase
+  end
 end
